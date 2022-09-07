4ee9e3ee240b84356b71941655d463f0a7664cb4
ljn7 <johnnirmal.16897@gmail.com>
2022-09-07 18:19:58 +0530

    modified and optimized

--- functions/player_game_logic.cpp
+++ functions/player_game_logic.cpp
@@ -16,155 +16,89 @@ bool gameLogic (vector<vector<bool>>& p_player_hash) {
         else if (p_player_hash[1][1])
             if (p_player_hash[2][2])
                 return 0;
-        // else if (p_player_hash[0][2])
-        //     if (p_player_hash[0][1])
-        //         return 0;
-        // else if (p_player_hash[2][2])
-        //     if (p_player_hash[1][1])
-        //         return 0;
-        // else if (p_player_hash[2][0])
-        //     if (p_player_hash[0][1])
-        //         return 0;
     }
     //checking table[0][1]
     if (p_player_hash[0][1]) {
         if (p_player_hash[0][0])
             if (p_player_hash[0][2])
                 return 0;
-        // else if (p_player_hash[0][2])
-        //     if (p_player_hash[0][0])
-        //         return 0;
         else if (p_player_hash[1][1])
             if (p_player_hash[2][1])
                 return 0;
-        // else if (p_player_hash[2][1])
-        //     if (p_player_hash[1][1])
-        //         return 0;
     }
     //checking table[0][2]
     if (p_player_hash[0][2]) {
         if (p_player_hash[0][1])
             if (p_player_hash[0][0])
                 return 0;
-        // else if (p_player_hash[0][0])
-        //     if (p_player_hash[0][1])
-        //         return 0;
         else if (p_player_hash[1][1])
             if (p_player_hash[2][0])
                 return 0;
         else if (p_player_hash[1][2])
             if (p_player_hash[2][2])
                 return 0;
-        // else if (p_player_hash[2][2])
-        //     if (p_player_hash[1][2])
-        //         return 0;
-        // else if (p_player_hash[2][0])
-        //     if (p_player_hash[1][1])
-        //         return 0;
     }
     //checking table[1][0]
     if (p_player_hash[1][0]) {
         if (p_player_hash[0][0])
             if (p_player_hash[2][0])
                 return 0;
-        // else if (p_player_hash[2][0])
-        //     if (p_player_hash[0][0])
-        //         return 0;
         else if (p_player_hash[1][1])
             if (p_player_hash[1][2])
                 return 0;
-        // else if (p_player_hash[1][2])
-        //     if (p_player_hash[1][1])
-        //         return 0;
     }
     if (p_player_hash[1][1]) {
         if (p_player_hash[0][0])
             if (p_player_hash[2][2])
                 return 0;
-        // else if (p_player_hash[2][2])
-        //     if (p_player_hash[0][0])
-        //         return 0;
         else if (p_player_hash[0][1])
             if (p_player_hash[2][1])
                 return 0;
-        // else if (p_player_hash[2][1])
-        //     if (p_player_hash[0][1])
-        //         return 0;
         else if (p_player_hash[0][2])
             if (p_player_hash[2][0])
                 return 0;
-        // else if (p_player_hash[2][0])
-        //     if (p_player_hash[0][2])
-        //         return 0;
     }
     //checking table[1][2]
     if (p_player_hash[1][2]) {
         if (p_player_hash[0][2])
             if (p_player_hash[2][2])
                 return 0;
-        // else if (p_player_hash[2][2])
-        //     if (p_player_hash[0][2])
-        //         return 0;
         else if (p_player_hash[1][0])
             if (p_player_hash[1][1])
                 return 0;
-        // else if (p_player_hash[1][1])
-        //     if (p_player_hash[1][0])
-        //         return 0;
     }
     //checking table[2][0]
     if (p_player_hash[2][0]) {
         if (p_player_hash[1][0])
             if (p_player_hash[0][0])
                 return 0;
-        // else if (p_player_hash[0][0])
-        //     if (p_player_hash[1][0])
-        //         return 0;
         else if (p_player_hash[1][1])
             if (p_player_hash[0][2])
                 return 0;
         else if (p_player_hash[2][1])
             if (p_player_hash[2][2])
                 return 0;
-        // else if (p_player_hash[2][2])
-        //     if (p_player_hash[2][1])
-        //         return 0;
     }
     //checking table[2][1]
     if (p_player_hash[2][1]) {
         if (p_player_hash[2][0])
             if (p_player_hash[2][2])
                 return 0;
-        // else if (p_player_hash[2][2])
-        //     if (p_player_hash[2][0])
-        //         return 0;
         else if (p_player_hash[1][1])
             if (p_player_hash[0][1])
                 return 0;
-        // else if (p_player_hash[0][1])
-        //     if (p_player_hash[1][1])
-        //         return 0;
     }
     //checking table[2][2]
     if (p_player_hash[2][2]) {
         if (p_player_hash[2][1])
             if (p_player_hash[2][0])
                 return 0;
-        // else if (p_player_hash[2][0])
-        //     if (p_player_hash[2][1])
-        //         return 0;
         else if (p_player_hash[1][1])
             if (p_player_hash[0][0])
                 return 0;
-        // else if (p_player_hash[0][0])
-        //     if (p_player_hash[1][1])
-        //         return 0;
         else if (p_player_hash[1][2])
             if (p_player_hash[0][2])
                 return 0;
-        // else if (p_player_hash[0][2])
-        //     if (p_player_hash[1][2])
-        //         return 0;
     }
     
     return 1;
