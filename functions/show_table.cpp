bf7e9dd65f69365d855f8a3bf6f813116978f274
ljn7 <johnnirmal.16897@gmail.com>
2022-09-07 18:16:24 +0530

    updated show_table()

--- functions/show_table.cpp
+++ functions/show_table.cpp
@@ -5,9 +5,11 @@ using std::vector;
 
 void show_table(const vector<vector<char>>& p_table) {
 
+    std::cout << "    1      2      3 " << std::endl; 
     for (int i = 0; i < 3; i++) {
+        std::cout << i + 1;
         for (int j = 0; j < 3; j++) 
             std::cout << " | " << p_table[i][j] << " | ";
-        std::cout << std::endl;
+        std::cout << std::endl << std::endl;
     }
 }
