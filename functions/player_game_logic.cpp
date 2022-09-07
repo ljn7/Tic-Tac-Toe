b2cab1e80f420886c86e5f3b47abe2736329f2e0
ljn7 <johnnirmal.16897@gmail.com>
2022-09-07 12:46:29 +0530

    subscript typo on game logic

--- functions/player_game_logic.cpp
+++ functions/player_game_logic.cpp
@@ -59,7 +59,7 @@ bool gameLogic (vector<vector<bool>>& p_player_hash) {
             if (p_player_hash[1][2])
                 return 0;
         else if (p_player_hash[2][0])
-            if (p_player_hash[0][1])
+            if (p_player_hash[1][1])
                 return 0;
     }
     //checking table[1][0]
